52ac7618f34a7b085892a2ca584dcb5a91e792af
Gregory Kanevsky <gregory.kanevsky@teradata.com>
2014-01-14 13:10:22 -0600

    updated package dependencies and imports

--- R/toaster.R
+++ R/toaster.R
@@ -1,6 +1,7 @@
-#' toaster: analytical and visualization toolbox for Teradata Aster Analytical Platform.
+#' toaster: analytical and visualization toolbox for Teradata Aster Discovery 
+#' and Big Data Analytics Platform.
 #' 
 #' @name toaster
 #' @docType package
-#' @import RODBC plyr reshape2 ggplot2 scales RColorBrewer wordcloud
+#' @import RODBC plyr reshape2 ggplot2 scales RColorBrewer wordcloud ggmap
 NULL

9513eec8eba7bc80d755ee070c359099eb54abf7
Gregory Kanevsky <gregory.kanevsky@teradata.com>
2014-02-24 22:42:48 -0600

    error handling and documentation

--- R/computeBarchart.R
+++ R/computeBarchart.R
@@ -1,21 +1,25 @@
 require(RODBC)
 
-#' Compute Barchart for a category and arbitrary number of aggregates.
+#' Compute one or more sets of values among category class.
 #' 
-#' Computes data suitable for bar chart plotting with single category corresponding to a column or 
-#' an expression. Function can compute arbitrary number of aggregates.
+#' Compute value(s) across category class represented by the table 
+#' column being compared. Category class usually is of character, temporal, 
+#' or discrete type. Category values are computed as aggregates across 
+#' categories utilising SQL \code{GROUP BY <class>}. Aggregates may include 
+#' any SQL expressions allowed in \code{GROUP BY} with the category 
+#' class column. 
 #' 
 #' @param channel connection object as returned by \code{\link{odbcConnect}}
 #' @param tableName table name
-#' @param category column name or expression corresponding to bar chart category. Name may be 
+#' @param category column name or expression associated with categories. Name may be 
 #'   valid SQL expression and can contain otional alias (e.g. \code{"UPPER(car_make) make"})
-#' @param aggregates SQL aggregates to compute. Aggregates may have optional aliases like in \code{"AVG(era) avg_era"}
+#' @param aggregates SQL aggregates to compute. Each aggregate corresponds to category value. 
+#'   Aggregates may have optional aliases like in \code{"AVG(era) era"}
 #' @param by for optional grouping by one or more columns for faceting or alike (effectively these elements
-#'   will be part of \code{GROUP BY ...})
-#' @param percent logical: 
+#'   will be part of \code{GROUP BY ...}) 
 #' @param where SQL WHERE clause limiting data from the table (use SQL as if in WHERE clause but omit keyword WHERE)
 #' @param orderBy list of column names, aliases, references or their combinations to use in SQL \code{ORDER BY} 
-#' clause
+#'   clause. Use in combination with \code{top} below.
 #' @param top if specified indicates number of bars to include in bar plot. In combination with \code{orderBy} 
 #'   it works as computing first \code{top} results.
 #' @param withMelt logical if TRUE then uses \pkg{reshape2} \code{\link{melt}} to transform result data frame
@@ -24,14 +28,15 @@ require(RODBC)
 #'   anything else be converted to factors?
 #' @param test logical: if TRUE show what would be done, only (similar to parameter \code{test} in \link{RODBC} 
 #'   functions like \link{sqlQuery} and \link{sqlSave}).
-#' 
+#' @return Data frame to use for bar chart plots with \code{\link{createHistogram}}.
 #' @export
-#' @seealso computeHistogram
+#' @seealso \code{\link{computeHistogram}}, \code{\link{createHistogram}}
 #' 
 #' @examples
 #' \donttest{
 #' # Compute average team season era, walks, and hits for each decade starting with 1980
-#' computeBarchart(asterConn, "teams_enh", "teamid team", c("avg(era) era", "avg(bb) bb", "avg(h) h"), 
+#' computeBarchart(channel=conn, "teams_enh", "teamid team", 
+#'                 aggregates=c("avg(era) era", "avg(bb) bb", "avg(h) h"), 
 #'                 where="yearid >=1980", by=c("decadeid"))
 #'                 
 #' # multipe aggregates in the same bar chart (with melt)
@@ -45,7 +50,7 @@ require(RODBC)
 #'                      where="yearid >= 1990 and lgid='AL'", by="decadeid", withMelt=TRUE)
 #' }
 computeBarchart <- function(channel, tableName, category,
-                            aggregates = "COUNT(*) cnt", percent = FALSE,
+                            aggregates = "COUNT(*) cnt", 
                             where = NULL, orderBy = NULL, top = NULL, by = NULL, 
                             withMelt = FALSE,
                             stringsAsFactors = FALSE, test = FALSE) {
@@ -56,6 +61,10 @@ computeBarchart <- function(channel, tableName, category,
   
   if (missing(category) | length(category) != 1) {
     stop("Bar chart must have exactly one category.")
+  }
+  
+  if (is.null(aggregates) || length(aggregates) < 1) {
+    stop("Must have at least one aggregate defined.")
   }
   
   where_clause = makeWhereClause(where)

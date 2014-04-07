c8be7f63fd022f4195b3b9340b58a5976f871399
Gregory Kanevsky <gregory.kanevsky@teradata.com>
2014-04-07 15:24:50 -0500

    added more examples.

--- test/test-baseball-computeLm.R
+++ test/test-baseball-computeLm.R
@@ -7,3 +7,9 @@ close(conn)
 conn = odbcConnect(dsn, uid, pwd)
 
 model1 = computeLm(channel=conn, tableName="batting_enh", expr= ba ~ rbi + bb + so)
+
+modelNL = computeLm(channel=conn, tableName="pitching_enh", expr= era ~ er + hr + bb + so, 
+                    where = "yearid >= 2000 and lgid = 'NL'")
+
+modelAL = computeLm(channel=conn, tableName="pitching_enh", expr= era ~ er + hr + bb + so, 
+                    where = "yearid >= 2000 and lgid = 'AL'")

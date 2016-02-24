c9b847cee0545ebc95367f0296b3ccbeb8b3c8de
Gregory Kanevsky <gregory.kanevsky@teradata.com>
2016-02-23 21:19:59 -0600

    broke up graph functions tests into sep.files

--- tests/testthat/test-computeGraph.R
+++ tests/testthat/test-computeGraph.R
@@ -10,8 +10,6 @@ test_that("toaGraph throws errors", {
   
   expect_error(toaGraph(NULL, "edges"),
                "Both vertices and edges must be defined.")
-  
-  
 })
 
 test_that("computeGraph throws errors", {
@@ -22,7 +20,7 @@ test_that("computeGraph throws errors", {
   expect_error(computeGraph(NULL, toaGraph("vs", "es"), test=TRUE),
                "Must provide allTables when test==TRUE.")
   
-  expect_error(computeGraph(NULL, toaGraph("vs", "es"), v=logical(1), 
+  expect_error(computeGraph(NULL, toaGraph("vs", "es"), v=list(logical(1)), 
                             allTables = data.frame(TABLE_NAME=c("vs","es"), stringsAsFactors = FALSE), test=TRUE),
                ".*Values must be either numeric or character only.")
 })
@@ -74,7 +72,7 @@ test_that("computeGraph works properly", {
                             WHERE source IN (SELECT id FROM vertices WHERE id IN (1, 2, 3) )
                               AND target IN (SELECT id FROM vertices WHERE id IN (1, 2, 3) ) ")
   
-  expect_equal_normalized(computeGraph(NULL, simplestGraph, v=c(1,2,3),
+  expect_equal_normalized(computeGraph(NULL, simplestGraph, v=list(1,2,3),
                                        allTables = data.frame(TABLE_NAME=c("vertices","edges"), stringsAsFactors = FALSE),
                                        test=TRUE),
                           "-- Edges Select
@@ -128,7 +126,7 @@ test_that("computeGraph works properly", {
                                                 "name", "name1", "name2",
                                                 vertexAttrnames = "role",
                                                 vertexWhere = "role = 'Actor'"),
-                                       v = c("Cruz", "Pacino", "Beluci", "Portman"),
+                                       v = list("Cruz", "Pacino", "Beluci", "Portman"),
                                        allTables = data.frame(TABLE_SCHEM=c("graph","graph"),
                                                               TABLE_NAME=c("films_vertices","films_edges")),
                       test=TRUE),
@@ -143,7 +141,8 @@ test_that("computeGraph works properly", {
                       --
                       -- Vertices Select
                       SELECT name, role FROM graph.films_vertices
-                       WHERE (role = 'Actor') AND name IN ('Cruz', 'Pacino', 'Beluci', 'Portman') ")
+                       WHERE (role = 'Actor') AND name IN ('Cruz', 'Pacino', 'Beluci', 'Portman') ",
+                      label="With vertex attr, vertex where, v list")
   
   expect_equal_normalized(computeGraph(NULL,
                                        toaGraph("graph.films_vertices", "graph.films_edges", FALSE,
@@ -151,7 +150,7 @@ test_that("computeGraph works properly", {
                                                 vertexAttrnames = "role",
                                                 vertexWhere = "role = 'Actor'"),
                                        edgeWhere = "years > 0",
-                                       v = c("Cruz", "Pacino", "Beluci", "Portman"),
+                                       v = list("Cruz", "Pacino", "Beluci", "Portman"),
                                        allTables = data.frame(TABLE_SCHEM=c("graph","graph"),
                                                               TABLE_NAME=c("films_vertices","films_edges")),
                       test=TRUE),
@@ -167,183 +166,55 @@ test_that("computeGraph works properly", {
                       --
                       -- Vertices Select
                       SELECT name, role FROM graph.films_vertices
-                       WHERE (role = 'Actor') AND name IN ('Cruz', 'Pacino', 'Beluci', 'Portman') ")
+                       WHERE (role = 'Actor') AND name IN ('Cruz', 'Pacino', 'Beluci', 'Portman') ",
+                      label="With vertex attr, vertex where, edge where, v list")
   
-})
-
-
-test_that("computeEgoGraph works properly", {
-  
-  expect_equal_normalized(computeEgoGraph(NULL, 
-                                          toaGraph("graph.films_vertices", "graph.films_edges", FALSE,
-                                                   "name", "name1", "name2"), 
-                                          ego='Keanu Reeves', createDistanceAttr = FALSE,
-                                          allTables = data.frame(TABLE_SCHEM=c("graph","graph"),
+  expect_equal_normalized(computeGraph(NULL,
+                                       toaGraph("graph.films_vertices", "graph.films_edges", FALSE,
+                                                "name", "name1", "name2",
+                                                vertexAttrnames = "role"),
+                                       v = "SELECT name FROM graph.films_vertices WHERE name like '%Bill%' or name like '%Tom%'",
+                                       allTables = data.frame(TABLE_SCHEM=c("graph","graph"),
                                                               TABLE_NAME=c("films_vertices","films_edges")),
-                                          test=TRUE),
-"BEGIN;
---
--- Create temp table of the shortest paths from ego vertices
-CREATE TEMP FACT TABLE egographtemp 
-       DISTRIBUTE BY HASH(source) 
-       AS
-       SELECT source, target, distance FROM AllPairsShortestPath(
-         ON (SELECT name 
-       FROM graph.films_vertices ) AS vertices PARTITION BY name
-         ON (SELECT name1, name2 
-         FROM graph.films_edges ) AS edges PARTITION BY name1
-         ON (SELECT name 
-       FROM graph.films_vertices WHERE name IN ('Keanu Reeves')  ) AS sources PARTITION BY name
-         TARGETKEY('name2')
-         DIRECTED('false')
-         MAXDISTANCE('1')
-       );
---
--- Edges Select
-SELECT e.*
-         FROM (SELECT name1, name2 
-         FROM graph.films_edges ) e 
-        WHERE name1 IN (SELECT target FROM egographtemp WHERE source = 'Keanu Reeves')
-          AND name2 IN (SELECT target FROM egographtemp WHERE source = 'Keanu Reeves')
-       UNION
-       SELECT e.*
-         FROM (SELECT name1, name2 
-         FROM graph.films_edges ) e
-        WHERE name1 = 'Keanu Reeves'
-           OR name2 = 'Keanu Reeves';
---
-END")
+                      test=TRUE),
+                      
+                      "-- Edges Select
+                      SELECT name1, name2 
+                        FROM graph.films_edges
+                       WHERE name1 IN (SELECT name FROM graph.films_vertices
+                                        WHERE name IN (SELECT name FROM graph.films_vertices WHERE name like '%Bill%' or name like '%Tom%') )
+                         AND name2 IN (SELECT name FROM graph.films_vertices 
+                                        WHERE name IN (SELECT name FROM graph.films_vertices WHERE name like '%Bill%' or name like '%Tom%') ) ;
+                      --
+                      -- Vertices Select
+                      SELECT name, role FROM graph.films_vertices
+                       WHERE name IN (SELECT name FROM graph.films_vertices WHERE name like '%Bill%' or name like '%Tom%') ",
+                      label="With vertex attr, v SELECT")
   
-  expect_equal_normalized(computeEgoGraph(NULL, 
-                                          toaGraph("graph.films_vertices", "graph.films_edges", FALSE,
-                                                   "name", "name1", "name2"), 
-                                          ego='Keanu Reeves',
-                                          allTables = data.frame(TABLE_SCHEM=c("graph","graph"),
+  expect_equal_normalized(computeGraph(NULL,
+                                       toaGraph("graph.films_vertices", "graph.films_edges", FALSE,
+                                                "name", "name1", "name2",
+                                                vertexAttrnames = "role",
+                                                vertexWhere = "role = 'Actor'"),
+                                       v = "SELECT name FROM graph.films_vertices WHERE name like '%Bill%' or name like '%Tom%'",
+                                       allTables = data.frame(TABLE_SCHEM=c("graph","graph"),
                                                               TABLE_NAME=c("films_vertices","films_edges")),
-                                          test=TRUE),
-"BEGIN;
---
--- Create temp table of the shortest paths from ego vertices
-CREATE TEMP FACT TABLE egographtemp 
-       DISTRIBUTE BY HASH(source) 
-       AS
-       SELECT source, target, distance FROM AllPairsShortestPath(
-         ON (SELECT name 
-       FROM graph.films_vertices ) AS vertices PARTITION BY name
-         ON (SELECT name1, name2 
-         FROM graph.films_edges ) AS edges PARTITION BY name1
-         ON (SELECT name 
-       FROM graph.films_vertices WHERE name IN ('Keanu Reeves')  ) AS sources PARTITION BY name
-         TARGETKEY('name2')
-         DIRECTED('false')
-         MAXDISTANCE('1')
-       );
---
--- Edges Select
-SELECT e.*
-         FROM (SELECT name1, name2 
-         FROM graph.films_edges ) e 
-        WHERE name1 IN (SELECT target FROM egographtemp WHERE source = 'Keanu Reeves')
-          AND name2 IN (SELECT target FROM egographtemp WHERE source = 'Keanu Reeves')
-       UNION
-       SELECT e.*
-         FROM (SELECT name1, name2 
-         FROM graph.films_edges ) e
-        WHERE name1 = 'Keanu Reeves'
-           OR name2 = 'Keanu Reeves';
---
--- Vertices Select
-SELECT v.*, eg.distance __distance_attr__ 
-           FROM egographtemp eg JOIN
-                (SELECT name 
-       FROM graph.films_vertices ) v ON (eg.target = v.name)
-          WHERE eg.source = 'Keanu Reeves'
-         UNION
-         SELECT v.*, 0 __distance_attr__ 
-           FROM egographtemp eg JOIN
-                (SELECT name 
-       FROM graph.films_vertices ) v ON (eg.source = v.name)
-          WHERE eg.source = 'Keanu Reeves';
---
-END")
-  
-  expect_equal_normalized(computeEgoGraph(conn, 
-                                          toaGraph("graph.films_vertices", "graph.films_edges", FALSE,
-                                                   "name", "name1", "name2",
-                                                   vertexAttrnames = "role", edgeAttrnames = c("weight","years")),
-                ego = c('Keanu Reeves','Takeshi Kitano'), order = 2,
-                test=TRUE, allTables = data.frame(TABLE_SCHEM=c("graph","graph"),
-                                               TABLE_NAME=c("films_vertices","films_edges"))),
-"BEGIN;
---
--- Create temp table of the shortest paths from ego vertices
-CREATE TEMP FACT TABLE egographtemp 
-       DISTRIBUTE BY HASH(source) 
-       AS
-       SELECT source, target, distance FROM AllPairsShortestPath(
-         ON (SELECT name, role 
-       FROM graph.films_vertices ) AS vertices PARTITION BY name
-         ON (SELECT name1, name2, weight, years 
-         FROM graph.films_edges ) AS edges PARTITION BY name1
-         ON (SELECT name, role 
-       FROM graph.films_vertices WHERE name IN ('Keanu Reeves', 'Takeshi Kitano')  ) AS sources PARTITION BY name
-         TARGETKEY('name2')
-         DIRECTED('false')
-         MAXDISTANCE('2')
-       );
---
--- Edges Select
-SELECT e.*
-         FROM (SELECT name1, name2, weight, years 
-         FROM graph.films_edges ) e 
-        WHERE name1 IN (SELECT target FROM egographtemp WHERE source = 'Keanu Reeves')
-          AND name2 IN (SELECT target FROM egographtemp WHERE source = 'Keanu Reeves')
-       UNION
-       SELECT e.*
-         FROM (SELECT name1, name2, weight, years 
-         FROM graph.films_edges ) e
-        WHERE name1 = 'Keanu Reeves'
-           OR name2 = 'Keanu Reeves';
---
--- Vertices Select
-SELECT v.*, eg.distance __distance_attr__ 
-           FROM egographtemp eg JOIN
-                (SELECT name, role 
-       FROM graph.films_vertices ) v ON (eg.target = v.name)
-          WHERE eg.source = 'Keanu Reeves'
-         UNION
-         SELECT v.*, 0 __distance_attr__ 
-           FROM egographtemp eg JOIN
-                (SELECT name, role 
-       FROM graph.films_vertices ) v ON (eg.source = v.name)
-          WHERE eg.source = 'Keanu Reeves';
---
--- Edges Select
-SELECT e.*
-         FROM (SELECT name1, name2, weight, years 
-         FROM graph.films_edges ) e 
-        WHERE name1 IN (SELECT target FROM egographtemp WHERE source = 'Takeshi Kitano')
-          AND name2 IN (SELECT target FROM egographtemp WHERE source = 'Takeshi Kitano')
-       UNION
-       SELECT e.*
-         FROM (SELECT name1, name2, weight, years 
-         FROM graph.films_edges ) e
-        WHERE name1 = 'Takeshi Kitano'
-           OR name2 = 'Takeshi Kitano';
---
--- Vertices Select
-SELECT v.*, eg.distance __distance_attr__ 
-           FROM egographtemp eg JOIN
-                (SELECT name, role 
-       FROM graph.films_vertices ) v ON (eg.target = v.name)
-          WHERE eg.source = 'Takeshi Kitano'
-         UNION
-         SELECT v.*, 0 __distance_attr__ 
-           FROM egographtemp eg JOIN
-                (SELECT name, role 
-       FROM graph.films_vertices ) v ON (eg.source = v.name)
-          WHERE eg.source = 'Takeshi Kitano';
---
-END")
+                      test=TRUE),
+                      
+                      "-- Edges Select
+                      SELECT name1, name2 
+                        FROM graph.films_edges
+                       WHERE name1 IN (SELECT name FROM graph.films_vertices 
+                                        WHERE (role = 'Actor') 
+                                          AND name IN (SELECT name FROM graph.films_vertices WHERE name like '%Bill%' or name like '%Tom%') )
+                         AND name2 IN (SELECT name FROM graph.films_vertices 
+                                        WHERE (role = 'Actor') 
+                                          AND name IN (SELECT name FROM graph.films_vertices WHERE name like '%Bill%' or name like '%Tom%') ) ;
+                      --
+                      -- Vertices Select
+                      SELECT name, role FROM graph.films_vertices
+                       WHERE (role = 'Actor') 
+                         AND name IN (SELECT name FROM graph.films_vertices WHERE name like '%Bill%' or name like '%Tom%') ",
+                      label="With vertex attr, vertex where, v SELECT")
   
 })

71fa017df29a680af2b1ccbea6a347ce82dbd273
Gregory Kanevsky <gregory.kanevsky@teradata.com>
2016-11-14 14:36:59 -0600

    updated ggplot and ggthemes to latest version

--- tests/testthat/test-createPopPyramid.R
+++ tests/testthat/test-createPopPyramid.R
@@ -3,9 +3,8 @@ context("createPopPyramid")
 test_that("createPopPyramid works", {
   data = data.frame(x=rep(1:10,2), y=rnorm(20, mean=100, sd=10), sex=rep(c('M','F'), each=10))
   
-  expect_warning(p <- ggplot_build(createPopPyramid(data=data, bin='x', count='y', 
-                                    divideBy='sex', values=c('M','F'))),
-                 "Stacking not well defined when ymin != 0")
+  p <- ggplot_build(createPopPyramid(data=data, bin='x', count='y', 
+                                    divideBy='sex', values=c('M','F')))
   
   expect_equal(nrow(p$data[[1]]), dim(data[data$sex=='M',])[[1]])
   expect_equal(nrow(p$data[[2]]), dim(data[data$sex=='F',])[[1]])
